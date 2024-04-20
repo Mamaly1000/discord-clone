fa03dc8791e74226c8cc2c783e6904ead19f2aac
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-04-20 06:39:36 +0330

    create a profile server action to get or create a profile , create setup page and add dialog component to ui folder

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,6 +1,16 @@
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const SetuptPage = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: { members: { some: { profileId: profile.id } } },
+  });
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
   return <div>setup your server</div>;
 };
 
